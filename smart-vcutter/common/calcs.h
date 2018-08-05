91409f9aa4a7644dae0164cc937d391bd1ea98e8
rodrigo antonio de araujo <rodjjo@gmail.com>
2018-08-05 15:16:26 -0300

    Comment what some complex functions are doing

--- smart-vcutter/common/calcs.h
+++ smart-vcutter/common/calcs.h
@@ -11,8 +11,14 @@ namespace vcutter {
 
 // Ensure the angle is interval 0..360
 float normalize_angle(float angle);
+
+// Convert px and py from screen coordinates to buffer (video frame) coordinates
 void buffer_coords(const int *vp, uint32_t video_w, uint32_t video_h, float *px, float *py);
+
+// Convert px and py from buffer (video frame) coordinates to screen coordinates
 void screen_coords(const int *vp, uint32_t video_w, uint32_t video_h, float *px, float *py);
+
+// Compute the coordinates where we should draw a video frame using glDrawPixels
 void raster_coordinates(const int *vp, uint32_t video_w, uint32_t video_h, float *pixel_zoom, float *raster_px, float *raster_py);
 
 // makes w and h smaller in order to not violate view port (vp) limits (if supplied, computed_fit_scale will have the scale used to fit)
